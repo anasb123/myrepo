78e72e5b2c23ba9fbe808dfe7a7b1e10a52594d1
anas <himiaryanas@gmail.com>
2022-10-27 20:20:58 +0300

    add message

--- add.php
+++ add.php
@@ -37,7 +37,7 @@ if(isset($_POST['Submit'])) {
 		$result = mysqli_query($mysqli, "INSERT INTO users(name,age,email) VALUES('$name','$age','$email')");
 		
 		//display success message
-		echo "<font color='green'>Data added successfully.";
+		echo "<font color='green'>Data added successfully yeeeeeeees anas.";
 		echo "<br/><a href='index.php'>View Result</a>";
 	}
 }
